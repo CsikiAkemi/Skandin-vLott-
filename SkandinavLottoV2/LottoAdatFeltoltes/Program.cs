083456a978e9ebae9abf1057f30f29602dda54df
CsikiAkemi <sisina.akemi@gmail.com>
2022-04-12 22:45:03 +0200

    Jáger Attila órája alapján készült

---
+++ SkandinavLottoV2/LottoAdatFeltoltes/Program.cs
@@ -0,0 +1,38 @@
+﻿using LottoAdatKapcsolat.Data;
+using LottoAdatKapcsolat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoAdatFeltoltes
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            LottoSzamContext db = new LottoSzamContext();
+            if (!db.LottoSzamok.Any())
+            {
+                string[] sorok = File.ReadAllLines(@"C:\Users\Akemi\Desktop\SkandinavLottoSzamok.csv");
+                LottoSzam lsz = null;
+                foreach (string sor in sorok)
+                {
+                    bool sikerult = true;
+                    try
+                    {
+                        lsz = new LottoSzam(sor);
+                    }
+                    catch (Exception)
+                    {
+                        sikerult = false;
+                    }
+                    if (sikerult) db.LottoSzamok.Add(lsz);
+                }
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
